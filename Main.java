3ddf64666508c97e24ab19ad10589d80151bb18e
smason03 <swmason03@gmail.com>
2024-07-25 16:11:33 -0500

    Adding all my .java Files

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+	public static void main(String[] args) {
+		String openingHours = "08:00";
+		String closingHours = "20:00";
+		
+		System.out.println("The park opens at " + openingHours +
+			" and closes at " + closingHours);
+	}
+}
+
+
+
+// public class Main {
+// 	public static void main(String[] args) {
+// 		String name = "Ralph";
+// 		String position = "Park Manager";
+// 		System.out.println("My name is " + name + 
+// 			" and I want to be a " + position + " in Mesozoic Eden." );
+// 	}
+// }
---
+++ Welcome.java
@@ -0,0 +1,14 @@
+public class Welcome { 
+	public static void main(String[] args) {
+		String name = "Steve";
+		String openingHours = "08:00";
+		String closingHours = "20:00";
+		
+		System.out.println("Welcome, " + name + " to the Mesozoic Eden!");
+
+		System.out.println("The park opens at " + openingHours +
+			" and closes at " + closingHours);
+
+		System.out.println("*** Mesozoic Eden is safe and secure. ***");
+	}
+}
